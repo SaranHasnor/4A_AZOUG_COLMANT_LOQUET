e0542f48bbb12a773b3b8f7169bc4f4b6aef0fe9
amauryESGI <amaury.colmant@wanadoo.fr>
2014-12-16 00:55:44 +0100

    Added class BlockPropertyX override

---
+++ Robots/Assets/Scripts/Behaviour/Block/Properties/BlockPropertyStick.cs
@@ -0,0 +1,7 @@
+﻿public class BlockPropertyStick : BlockProperty {
+    protected override void _Interact(ActionOnBlock action, string[] args = null) {
+        if (action != ActionOnBlock.Stick) {
+            // TODO : Completer algo property Stick
+        }
+    }
+}
