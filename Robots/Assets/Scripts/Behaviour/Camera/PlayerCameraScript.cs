0391bcb94339fcb8f95ecf91ea6f1abd82200a43
SaranHasnor <SaranHasnor@users.noreply.github.com>
2014-12-17 00:12:38 +0100

    Amélioration de la caméra, déplacement d'un fichier mal rangé

--- Robots/Assets/Scripts/Behaviour/Camera/PlayerCameraScript.cs
+++ Robots/Assets/Scripts/Behaviour/Camera/PlayerCameraScript.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class PlayerCameraScript : MonoBehaviour
 { // Manages the position of the camera and the player's camera-related input
+
+	[SerializeField]
+	private float _maxCameraZoomSpeed = 15.0f;
+
 	[SerializeField]
 	private float _maxCameraAngularSpeed = 90.0f;
 
@@ -14,21 +18,29 @@ public class PlayerCameraScript : MonoBehaviour
 	private float _desiredCameraDistance = 10.0f;
 
 	// The two following angles describe the angle created by the camera and the origin
-	private float _desiredCameraHorzAngle = 45.0f;
+	private float _desiredCameraHorzAngle = 45.0f; // TODO: Prevent this from being farther than half a loop away from us
 	private float _desiredCameraVertAngle = 45.0f;
 
 	void Update()
-	{
+	{ // I feel like this has gotten over-complicated over time
+		Vector3 cameraPos = this.transform.position - _anchorPoint;
+
 		Quaternion desiredAngle = Quaternion.Euler(-_desiredCameraVertAngle, -_desiredCameraHorzAngle, 0.0f);
-		Quaternion currentAngle = Quaternion.LookRotation(this.transform.position - _anchorPoint);
+		Quaternion currentAngle = Quaternion.LookRotation(cameraPos);
 
 		float diff = Quaternion.Angle(currentAngle, desiredAngle);
 		float maxDelta = Mathf.Clamp(diff * _magicNumber, 3.0f, _maxCameraAngularSpeed) * Time.deltaTime;
 		Quaternion targetAngle = Quaternion.RotateTowards(currentAngle, desiredAngle, maxDelta);
 
+		float currentCameraDistance = cameraPos.magnitude;
 		Vector3 desiredPosition = targetAngle * Vector3.forward * _desiredCameraDistance;
+		float diff2 = Vector3.Distance(this.transform.position, desiredPosition);
+		float maxDelta2 = Mathf.Clamp(diff2 * _magicNumber, 1.0f, _maxCameraZoomSpeed) * Time.deltaTime;
+		float targetCameraDistance = Vector3.MoveTowards(cameraPos, desiredPosition, maxDelta2).magnitude;
 
-		this.transform.position = desiredPosition;
+		Vector3 newPosition = targetAngle * Vector3.forward * targetCameraDistance;
+
+		this.transform.position = newPosition;
 		this.transform.LookAt(_anchorPoint); // I was going to recode it but I guess this works
 
 		UpdateInput();
@@ -52,5 +64,17 @@ public class PlayerCameraScript : MonoBehaviour
 		{
 			_desiredCameraVertAngle += 2.5f;
 		}
+
+		if (Input.GetKey(KeyCode.KeypadPlus))
+		{
+			_desiredCameraDistance -= 0.2f;
+		}
+		if (Input.GetKey(KeyCode.KeypadMinus))
+		{
+			_desiredCameraDistance += 0.2f;
+		}
+
+		_desiredCameraVertAngle = Mathf.Clamp(_desiredCameraVertAngle, -85.0f, 85.0f);
+		_desiredCameraDistance = Mathf.Clamp(_desiredCameraDistance, 1.0f, 50.0f);
 	}
 }
