48deb0209f7d9bc681c1d107cd0b4f80d3ab57b7
amauryESGI <amaury.colmant@wanadoo.fr>
2015-02-22 14:41:15 +0100

    Sup of using witch are not required by the code.

--- Robots/Assets/Scripts/Data/Actions/Abstract/EntityTargetedAction.cs
+++ Robots/Assets/Scripts/Data/Actions/Abstract/EntityTargetedAction.cs
@@ -1,6 +1,4 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 public abstract class EntityTargetedAction : EntityAction
 { // Action of an entity on an empty cell
