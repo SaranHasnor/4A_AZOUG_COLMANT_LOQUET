db5c85b5914975be0a0697f01401355b670a265f
amauryESGI <amaury.colmant@wanadoo.fr>
2015-03-04 19:08:09 +0100

    N/A

--- Robots/Assets/Scripts/UI/InGame/GUIRenderer.cs
+++ Robots/Assets/Scripts/UI/InGame/GUIRenderer.cs
@@ -4,35 +4,51 @@ using System.Collections.Generic;
 public class GUIRenderer : MonoBehaviour {
 
 	#region General
+	[SerializeField]
+	private GameObject _ButtonPlayPause;
+	[SerializeField]
+	private GameObject _ButtonReplay;
 	#endregion General
 
 	#region Action
-	[SerializeField] private GameObject _ButtonMove;
-	[SerializeField] private GameObject _ButtonPush;
-	[SerializeField] private GameObject _ButtonWait;
+	[SerializeField]
+	private GameObject _ButtonMove;
+	[SerializeField]
+	private GameObject _ButtonPush;
+	[SerializeField]
+	private GameObject _ButtonWait;
 
 
-	[SerializeField] private GameObject _ButtonMoveUp;
-	[SerializeField] private GameObject _ButtonMoveDown;
-	[SerializeField] private GameObject _ButtonMoveLeft;
-	[SerializeField] private GameObject _ButtonMoveRight;
+	[SerializeField]
+	private GameObject _ButtonMoveUp;
+	[SerializeField]
+	private GameObject _ButtonMoveDown;
+	[SerializeField]
+	private GameObject _ButtonMoveLeft;
+	[SerializeField]
+	private GameObject _ButtonMoveRight;
 
-	[SerializeField] private GameObject _ButtonPushUp;
-	[SerializeField] private GameObject _ButtonPushDown;
-	[SerializeField] private GameObject _ButtonPushLeft;
-	[SerializeField] private GameObject _ButtonPushRight;
+	[SerializeField]
+	private GameObject _ButtonPushUp;
+	[SerializeField]
+	private GameObject _ButtonPushDown;
+	[SerializeField]
+	private GameObject _ButtonPushLeft;
+	[SerializeField]
+	private GameObject _ButtonPushRight;
 
-	[SerializeField] private GameObject _ButtonQueueDelete;
-	[SerializeField] private GameObject _ButtonQueueLeft;
-	[SerializeField] private GameObject _ButtonQueueRight;
+	[SerializeField]
+	private GameObject _ButtonQueueDelete;
+	[SerializeField]
+	private GameObject _ButtonQueueBefore;
+	[SerializeField]
+	private GameObject _ButtonQueueAfter;
 	#endregion Action
 
 	private MapEntity _selectedEntity;
 
-	private ActionQueue _selectedQueue
-	{
-		get
-		{
+	private ActionQueue _selectedQueue {
+		get {
 			return (_selectedEntity != null) ? GameData.currentState.actions[_selectedEntity.id] : null;
 		}
 	}
@@ -46,54 +62,44 @@ public class GUIRenderer : MonoBehaviour {
 
 	private Vector2 _timelineScrollPos;
 
-	void Start()
-	{
+	void Start() {
 		GameData.guiRenderer = this;
 		_selectedActionIndex = -1;
 	}
 
-	void OnGUI()
-	{
+	void OnGUI() {
 
 		if (GameData.gameMaster.isVictory()) {
 			DrawVictory();
 		} else if (GameData.gameMaster.isLose()) {
 			DrawLose();
-		}
-		else
-		{
-			if (_selectedEntity != null)
-			{
-				DrawActionTimeLine(_selectedQueue.actions, new Rect(0.0f, 0.7f*Screen.height, Screen.width, 0.3f*Screen.height));
-				if (_selectedActionIndex != -1)
-				{
+		} else {
+			if (_selectedEntity != null) {
+				DrawActionTimeLine(_selectedQueue.actions, new Rect(0.0f, 0.7f * Screen.height, Screen.width, 0.3f * Screen.height));
+				if (_selectedActionIndex != -1) {
 					DrawActionList(new Rect());
 				}
 			}
 			if (GUI.Button(
-				new Rect(Screen.width - 0.2f*Screen.width, 0.01f*Screen.height, 0.2f*Screen.width, 0.2f*Screen.height), "Play/Pause"))
-			{
+				new Rect(Screen.width - 0.2f * Screen.width, 0.01f * Screen.height, 0.2f * Screen.width, 0.2f * Screen.height), "Play/Pause")) {
 				GameData.timeMaster.ToggleRun();
 			}
 			if (GUI.Button(
-				new Rect(Screen.width - 0.2f*Screen.width, 0.21f*Screen.height, 0.2f*Screen.width, 0.2f*Screen.height), "Rewind"))
-			{
+				new Rect(Screen.width - 0.2f * Screen.width, 0.21f * Screen.height, 0.2f * Screen.width, 0.2f * Screen.height), "Rewind")) {
 
 			}
 		}
 		DrawTurn();
 	}
 
-	private void DrawActionList(Rect rect)
-	{
+	private void DrawActionList(Rect rect) {
 		/*foreach (System.Type type in _actionTypes)
 		{
 			
 		}*/
 	}
 
-	private void DrawActionTimeLine(List<EntityAction> timeline, Rect rect)
-	{
+	private void DrawActionTimeLine(List<EntityAction> timeline, Rect rect) {
 		float itemWidth = 70.0f;
 
 		GUI.Box(rect, GUIContent.none);
@@ -104,8 +110,7 @@ public class GUIRenderer : MonoBehaviour {
 		_timelineScrollPos = GUI.BeginScrollView(new Rect(0.0f, 0.0f, rect.width, rect.height), _timelineScrollPos, new Rect(0.0f, 0.0f, scrollAreaWidth, rect.height - 20.0f), true, false);
 
 		int cursor = 0;
-		foreach (EntityAction action in timeline)
-		{
+		foreach (EntityAction action in timeline) {
 			DrawAction(action, new Rect(cursor * itemWidth, 0.0f, itemWidth, rect.height - 20.0f), cursor);
 			cursor++;
 		}
@@ -116,22 +121,18 @@ public class GUIRenderer : MonoBehaviour {
 		GUI.EndGroup();
 	}
 
-	private void DrawAction(EntityAction action, Rect rect, int index)
-	{
-		if (GUI.Button(rect, action != null ? action.GetType().ToString() : null))
-		{
+	private void DrawAction(EntityAction action, Rect rect, int index) {
+		if (GUI.Button(rect, action != null ? action.GetType().ToString() : null)) {
 			//_selectedActionIndex = index;
-			if (action == null)
-			{ // Temporary
+			if (action == null) { // Temporary
 				_selectedQueue.SetAction(new EntityActionMove(_selectedEntity.id, new MapPosition(0, 0, 0)));
 			}
 		}
 	}
 
-	private void DrawVictory()
-	{
+	private void DrawVictory() {
 		GUI.Box(
-			new Rect(0.25f*Screen.width, 0.25f*Screen.height, 0.5f*Screen.width, 0.5f*Screen.height),
+			new Rect(0.25f * Screen.width, 0.25f * Screen.height, 0.5f * Screen.width, 0.5f * Screen.height),
 			"WINNER");
 	}
 
@@ -142,15 +143,12 @@ public class GUIRenderer : MonoBehaviour {
 	}
 
 	private void DrawTurn() {
-		if (GameData.gameMaster.GetMaxTurn() == -1)
-		{
+		if (GameData.gameMaster.GetMaxTurn() == -1) {
 			GUI.Label(
 				new Rect(0.49f * Screen.width, 0.01f * Screen.height, 0.02f * Screen.width, 0.05f * Screen.height),
 				GameData.timeMaster.GetTurn().ToString()
 				);
-		}
-		else
-		{
+		} else {
 			GUI.Label(
 				new Rect(0.45f * Screen.width, 0.01f * Screen.height, 0.05f * Screen.width, 0.05f * Screen.height),
 				GameData.timeMaster.GetTurn() + "/" + GameData.gameMaster.GetMaxTurn()
@@ -158,8 +156,61 @@ public class GUIRenderer : MonoBehaviour {
 		}
 	}
 
-	public void SelectEntity(MapEntity entity)
-	{
+	public void SelectEntity(MapEntity entity) {
 		_selectedEntity = entity;
 	}
+
+
+	public void OnPlayPause() {
+	}
+
+	public void OnReplay() {
+	}
+
+
+	public void OnMove() {
+	}
+
+	public void OnPush() {
+	}
+
+	public void OnWait() {
+	}
+
+
+	public void OnMoveUp() {
+	}
+
+	public void OnMoveDown() {
+	}
+
+	public void OnMoveLeft() {
+	}
+
+	public void OnMoveRight() {
+	}
+
+
+	public void OnPushUp() {
+	}
+
+	public void OnPushDown() {
+	}
+
+	public void OnPushLeft() {
+	}
+
+	public void OnPushRight() {
+	}
+
+
+	public void OnQueueDelete() {
+	}
+
+	public void OnQueueBefore() {
+	}
+
+	public void OnQueueAfter() {
+	}
+
 }
