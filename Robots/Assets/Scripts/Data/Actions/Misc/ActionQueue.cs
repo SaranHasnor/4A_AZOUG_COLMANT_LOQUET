a37a7ca74a618ccf7fe882b697b3ba6ed689d955
amauryESGI <amaury.colmant@wanadoo.fr>
2015-03-04 23:47:08 +0100

    N/A

--- Robots/Assets/Scripts/Data/Actions/Misc/ActionQueue.cs
+++ Robots/Assets/Scripts/Data/Actions/Misc/ActionQueue.cs
@@ -2,75 +2,64 @@
 using System.Collections.Generic;
 using System.Xml;
 
-public class ActionQueue
-{
+public class ActionQueue {
 	private List<EntityAction> queue;
 	private int cursor;
 
-	public int length
-	{
-		get
-		{
+	public int length {
+		get {
 			return queue.Count;
 		}
 	}
 
-	public List<EntityAction> actions
-	{
-		get
-		{
+	public List<EntityAction> actions {
+		get {
 			return new List<EntityAction>(queue);
 		}
 	}
 
-	public ActionQueue()
-	{
+	public ActionQueue() {
 		this.queue = new List<EntityAction>();
 		this.cursor = 0;
 	}
 
-	public void SetAction(EntityAction action, int timeOverride = -1)
-	{
+	public void SetAction(EntityAction action, int timeOverride = -1) {
 		int time = (timeOverride < 0) ? this.cursor : timeOverride;
 
-		while (queue.Count <= time)
-		{ // Set the new time frame by filling empty spaces with null actions
+		while (queue.Count <= time) { // Set the new time frame by filling empty spaces with null actions
 			queue.Add(null);
 		}
 
 		queue[time] = action;
 
-		if (timeOverride < 0) this.cursor++;
+		if (timeOverride < 0)
+			this.cursor++;
 	}
 
-	public void RemoveAction(int timeOverride = -1)
-	{
+	public void RemoveAction(int timeOverride = -1) {
 		int time = (timeOverride < 0) ? this.cursor : timeOverride;
 
-		if (time < queue.Count)
-		{
+		if (time < queue.Count) {
 			queue.RemoveAt(time);
 
-			if (timeOverride < 0) this.cursor--;
+			if (timeOverride < 0)
+				this.cursor--;
 		}
 	}
 
-	public void MoveAction(int source, int target)
-	{ // Useful?
+	public void MoveAction(int source, int target) { // Useful?
 
 	}
 
-	public EntityActionResult Run(int timeOverride = -1)
-	{
+	public EntityActionResult Run(int timeOverride = -1) {
 		int time = (timeOverride < 0) ? this.cursor : timeOverride;
 
-		if (time < queue.Count)
-		{
+		if (time < queue.Count) {
 			EntityActionResult res = queue[time].Run();
 
-			if (res != EntityActionResult.Repeat)
-			{
-				if (timeOverride < 0) this.cursor++;
+			if (res != EntityActionResult.Repeat) {
+				if (timeOverride < 0)
+					this.cursor++;
 			}
 
 			return res;
@@ -79,25 +68,21 @@ public class ActionQueue
 		return EntityActionResult.Success; // Ran out of actions, don't bother the boss with useless messages
 	}
 
-	public void SetCursor(int time, bool relative = false)
-	{
+	public void SetCursor(int time, bool relative = false) {
 		this.cursor = Mathf.Clamp(relative ? cursor + time : time, 0, queue.Count);
 	}
 
-	public int GetCursor()
-	{
+	public int GetCursor() {
 		return this.cursor;
 	}
 
-	public EntityAction GetAction(int timeOverride = -1)
-	{
+	public EntityAction GetAction(int timeOverride = -1) {
 		int time = (timeOverride < 0) ? this.cursor : timeOverride;
 
 		return queue[time];
 	}
 
-	public XmlDocument ToXml()
-	{ // Serialize all the actions we're holding and put them together
+	public XmlDocument ToXml() { // Serialize all the actions we're holding and put them together
 		XmlDocument doc = new XmlDocument();
 		XmlElement node = doc.CreateElement("queue");
 
@@ -105,26 +90,21 @@ public class ActionQueue
 
 		attributes.Add("id", queue[0].owner.id); // Temporary
 
-		foreach (KeyValuePair<string, string> attribute in attributes)
-		{
+		foreach (KeyValuePair<string, string> attribute in attributes) {
 			node.SetAttribute(attribute.Key, attribute.Value);
 		}
 
-		foreach (EntityAction action in this.queue)
-		{
+		foreach (EntityAction action in this.queue) {
 			node.AppendChild(action.ToXml());
 		}
 
 		return doc;
 	}
 
-	public static ActionQueue CreateFromXmlNode(XmlNode node)
-	{
+	public static ActionQueue CreateFromXmlNode(XmlNode node) {
 		ActionQueue result = new ActionQueue();
-		foreach (XmlNode actionNode in node)
-		{
-			if (actionNode.Name == "action")
-			{
+		foreach (XmlNode actionNode in node) {
+			if (actionNode.Name == "action") {
 				result.SetAction(EntityAction.CreateFromXmlNode(actionNode, node.Attributes["id"].Value));
 			}
 		}
@@ -136,11 +116,9 @@ public class ActionQueue
 		queue.Insert(cursor, action);
 	}
 
-	public void Swap(int index1, int index2)
-	{
-		if (index1 != 0 && index1 != actions.Count)
-		{
-			EntityAction tmp = actions[index1];
+	public void Swap(int index1, int index2) {
+		if (index1 >= 0 && index1 < actions.Count && index2 >= 0 && index2 < actions.Count) {
+			var tmp = actions[index1];
 			actions[index1] = actions[index2];
 			actions[index2] = tmp;
 		}
